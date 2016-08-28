af6350305a0d34a11be5443aaf5326e9704e9be1
presly808 <presly808@gmail.com>
2016-08-28 12:13:03 +0300

    week4 day2 part1

---
+++ src/ua/artcode/week4/day2/IBash.java
@@ -0,0 +1,28 @@
+package ua.artcode.week4.day2;
+
+import java.io.FileNotFoundException;
+import java.util.List;
+
+/**
+ * Created by serhii on 28.08.16.
+ */
+public interface IBash {
+
+    // show file content
+    String less(String path) throws FileNotFoundException;
+
+    void writeInto(String path, String content) throws FileNotFoundException;
+
+    void appendTo(String path, String content) throws FileNotFoundException;
+
+    // show files in current directory
+    List<String> ls(String currentDirPath);
+    // use File
+    // create new file
+    boolean touch(String path);
+
+    boolean delete(String path);
+
+    List<String> grep(String src, String keyWord);
+
+}
